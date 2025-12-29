b09effbc8c6a4d83e79462744abe332137de3e15
Ganesh_devops <gdevops@gmail.com>
2025-12-29 16:53:52 +0530

    up date code

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "app" {
+resource "aws_instance" "app_server" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
   key_name      = "devops-key"
@@ -19,4 +19,9 @@ resource "aws_instance" "app" {
   tags = {
     Name = "Prod-App-Server"
   }
+}
+
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
 }
